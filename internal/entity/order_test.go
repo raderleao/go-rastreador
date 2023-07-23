d8cdd6578f079c2388589259ec20acfe34b84976
raderleao <raderleao@hotmail.com>
2023-07-22 21:15:49 -0300

    first

---
+++ internal/entity/order_test.go
@@ -0,0 +1,22 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIfItGetsAnErrorIfIDIsBlank(t *testing.T) {
+	order := Order{}
+	assert.Error(t, order.Validate(), "id is requerid")
+}
+
+func TestFinalPrice(t *testing.T) {
+	order := Order{ID: "123", Price: 10.0, Tax: 1.0}
+	assert.NoError(t, order.Validate())
+	assert.Equal(t, "123", order.ID)
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 1.0, order.Tax)
+	order.CalculateFinalPrice()
+	assert.Equal(t, 11.0, order.FinalPrice)
+}
